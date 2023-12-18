af6d4a535ca19b3ca3179371916cce93891fa71c
BrayanCode11 <bangarita@unal.edu.co>
2023-12-18 15:39:27 -0500

    Begining proyect React Calculator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import "./App.css";
+import freeCodeCampLogo from "./images/freecodecamp-logo.png";
 
 function App() {
   return (
     <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img src={freeCodeCampLogo} alt="Logo de freeCodeCamp" className="freecodecamp-logo" />
+      </div>
     </div>
   );
 }
