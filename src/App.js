ebd51433beb56724ad0d66d8b2db9ab9b689e8af
BrayanCode11 <bangarita@unal.edu.co>
2023-12-18 15:58:46 -0500

    Calculadora conteiner

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ function App() {
   return (
     <div className="App">
       <div className="freecodecamp-logo-contenedor">
-        <img src={freeCodeCampLogo} alt="Logo de freeCodeCamp" className="freecodecamp-logo" />
+        <img
+          src={freeCodeCampLogo}
+          alt="Logo de freeCodeCamp"
+          className="freecodecamp-logo"
+        />
       </div>
+
+      <div className="contenedor-calculadora">
+
+      </div>
+
+
     </div>
   );
 }
